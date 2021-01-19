c28d9c7850af6d4254305a482d5a6368def22bde
alejandrodk <ottoabarriosp@hotmail.com>
2021-01-19 08:06:57 -0300

    Feature: Create repositories

---
+++ src/infrastructure/repositories/author.repository.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Author, AuthorDocument } from '../../domain/entities';
+import { AuthorCreateDTO } from '../../application/dtos/author/author-create.dto';
+import { Entities } from '../../domain/entities/entities.enum';
+
+@Injectable()
+export class AuthorRepository {
+  constructor(@InjectModel(Entities.Author) private readonly authorModel: Model<AuthorDocument>) {}
+
+  async findAll(): Promise<Author[]> {
+    const results: Author[] = await this.authorModel.find().exec();
+    return results;
+  }
+
+  async persist(authorCreateDTO: AuthorCreateDTO): Promise<Author> {
+    const newAuthor = new this.authorModel(authorCreateDTO);
+    return newAuthor.save();
+  }
+}
