9e1113dbf31078da68c7e42e31dc381347efc7ac
alejandrodk <ottoabarriosp@hotmail.com>
2021-01-19 08:02:45 -0300

    Feature: Create orm module for db connection

--- src/infrastructure/modules/app.module.ts
+++ src/infrastructure/modules/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { OrmModule } from '../database/orm';
 import { AppController } from '../../application/controllers/app.controller';
-import { configuration } from '../config/env.objects';
+import { configuration, EnvObjects, MongoOptions } from '../config/env.objects';
 import { validate } from '../config/env.validation';
 @Module({
   imports: [
@@ -12,6 +13,14 @@ import { validate } from '../config/env.validation';
       cache: true,
       expandVariables: true,
     }),
+    OrmModule.forRootAsync({
+        imports: [ConfigModule],
+        useFactory: async (configService: ConfigService) => {
+          const data = configService.get<MongoOptions>(EnvObjects.MONGO_OPTIONS);
+          return { uri: data?.host, ...data?.options };
+        },
+        inject: [ConfigService],
+      }),
   ],
   controllers: [AppController],
 })
